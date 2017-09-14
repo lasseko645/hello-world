623557627480959fddcb242def640f67a623b028
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2017-09-14 13:07:33 +0200

    jeg addede nogle sætninger for at checke hvordan printline koden fungere
    i fremtiden er planen at sætte et link ind men jeg ville bar lige tjekke om det her virker først

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,10 @@
 public class Welcome {
 	public static void main(String[] args){
-		System.out.println("welcome to java"
-				  "hej"
-				  "im exited");
+		System.out.println("welcome to java");
+		System.out.println("ill show you something fun");
+		System.out.println("it might hurt you heart");
+		System.out.println("consider yourself warned");
+		
+		
  }
 }
