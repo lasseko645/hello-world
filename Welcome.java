d18e11bf7bb82403319b56b1adf6c47ef3e91e8e
lasseko645 <31951011+lasseko645@users.noreply.github.com>
2017-09-14 13:02:12 +0200

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,7 @@
 public class Welcome {
 	public static void main(String[] args){
-		System.out.println("welcome to java");
+		System.out.println("welcome to java"
+				  "hej"
+				  "im exited");
  }
 }
